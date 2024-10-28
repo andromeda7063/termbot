02ad9ee06864fa9bf4bc2a8abae039cf71567177
andromeda <monika.ms1917@gmail.com>
2024-10-28 08:16:19 +0530

    idk

--- scripts/calc.py
+++ scripts/calc.py
@@ -2,19 +2,18 @@
 # should take 5+33-2*4 and return 30
 # do again after DSA
 
-def calc():
+def calc(a):
 
     opers = ['+', '-', '*', '/', '**']
     digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     flag = True
 
-    a = input()
-
-    for i in a:
-        if i not in opers and i not in digits:
-            flag = False
-            break
+    if any(i not in opers and i not in digits for i in a):
+        flag = False
 
     if flag:
-        print(eval(a))
+        return eval(a)
+
+a = input()
+print(calc(a))
